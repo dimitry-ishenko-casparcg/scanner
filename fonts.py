4a1a0d22f52095912b6ccc713b69600061baad73
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-09-18 21:19:56 -0400

    fonts: implement crawl

--- fonts.py
+++ fonts.py
@@ -13,9 +13,22 @@ class FontHandler(FileSystemEventHandler):
 
     def crawl(self):
         print("[fonts] Scanning", self.watch_path)
-        for file_path in self.watch_path.rglob("*"):
-            if file_path.is_file():
-                pass
+        disk_fonts = {}
+        for path in self.watch_path.rglob("*"):
+            if path.is_file():
+                name = get_name(path, self.watch_path, extens)
+                if name: disk_fonts[name] = str(path)
+
+        store_names = { name for name, _ in self.store.get_fonts() }
+        disk_names = set(disk_fonts.keys())
+
+        add_names = disk_names - store_names
+        remove_names = store_names - disk_names
+
+        add_fonts = [ (name, disk_fonts[name]) for name in add_names ]
+        if add_fonts or remove_names:
+            self.store.add_remove_fonts(add_fonts, remove_names)
+            print(f"[fonts] Added {len(add_fonts)}, removed {len(remove_names)} fonts.")
 
     def _add(self, path: str):
         if name := get_name(Path(path), self.watch_path, extens):
