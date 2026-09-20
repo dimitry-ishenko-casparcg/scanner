f434654bb9adaa7a13362bbaa035c6e0652536aa
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-09-19 20:16:54 -0400

    config: refactor

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ class Config:
         self.http_addr = "0.0.0.0"
         self.http_port = 8000
 
-        def get_path(config_path: Path, parent_node, tag_name, fallback):
-            node = parent_node.find(tag_name) if parent_node else None
+        def get_path(config_path: Path, parent, tag, fallback):
+            node = parent.find(tag) if parent else None
             path = node.text.strip() if node is not None and node.text else fallback
 
             # Compute 'path' relative to 'config_path'.
             # If 'path' is absolute, 'config_path' will be ignored.
             return config_path.parent / Path(path)
 
-        root_node = et.parse(config_path).getroot()
-        if root_node.tag != "configuration":
-            raise ValueError(f"Invalid root tag <{root_node.tag}>.")
-        paths_node = root_node.find("paths")
+        root = et.parse(config_path).getroot()
+        if root.tag != "configuration":
+            raise ValueError(f"Invalid root tag <{root.tag}>.")
+        paths = root.find("paths")
 
-        self.font_path = get_path(config_path, paths_node, "font-path", "./font")
-        self.media_path = get_path(config_path, paths_node, "media-path", "./media")
-        self.template_path = get_path(config_path, paths_node, "template-path", "./template")
+        self.font_path = get_path(config_path, paths, "font-path", "./font")
+        self.media_path = get_path(config_path, paths, "media-path", "./media")
+        self.template_path = get_path(config_path, paths, "template-path", "./template")
