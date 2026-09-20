41f1c669e9843c070a2f6a6bbba0456e4d5b17c9
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-09-20 10:51:25 -0400

    fonts: inherit from EventHandler

--- fonts.py
+++ fonts.py
@@ -1,15 +1,11 @@
+from handler import EventHandler
 from pathlib import Path
 from store import Store
 from util import get_name_type
-from watchdog.events import FileSystemEventHandler
 
 types = {"ttf", "otf", "woff", "woff2"}
 
-class FontHandler(FileSystemEventHandler):
-    def __init__(self, watch_path: Path, store: Store):
-        self.watch_path = watch_path
-        self.store = store
-        super().__init__()
+class FontHandler(EventHandler):
 
     def crawl(self):
         print(f"[fonts] Scanning {self.watch_path}")
@@ -34,7 +30,7 @@ class FontHandler(FileSystemEventHandler):
             print(f"[fonts] Adding {len(add_names)} fonts")
             self.store.add_fonts([ (name, on_disk[name]) for name in add_names ])
 
-    def _add(self, path: Path):
+    def add(self, path: Path):
         name, _ = get_name_type(path, self.watch_path, types)
         if not name: return
 
@@ -42,25 +38,9 @@ class FontHandler(FileSystemEventHandler):
         print(f"[fonts] Adding {name} => {fullpath}")
         self.store.add_fonts([ (name, fullpath) ])
 
-    def _remove(self, path: Path):
+    def remove(self, path: Path):
         name, _ = get_name_type(path, self.watch_path, types)
         if not name: return
 
         print(f"[fonts] Removing {name}")
         self.store.remove_fonts([ name ])
-
-    def on_created(self, event):
-        if not event.is_directory: self._add(Path(event.src_path))
-
-    def on_modified(self, event):
-        if not event.is_directory: self._add(Path(event.src_path))
-
-    def on_deleted(self, event):
-        if not event.is_directory: self._remove(Path(event.src_path))
-        else: self.crawl()
-
-    def on_moved(self, event):
-        if not event.is_directory:
-            self._remove(Path(event.src_path))
-            self._add(Path(event.dest_path))
-        else: self.crawl()
