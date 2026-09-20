16e3ec16c9b4c27982854fe1af178360d7eed6ca
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-09-19 20:31:52 -0400

    util: refactor get_name_type()

--- util.py
+++ util.py
@@ -3,8 +3,7 @@ from pathlib import Path
 def get_name_type(path: Path, base_path: Path, types):
     type_ = path.suffix.lower()[1:]
     if not type_ in types: return None, None
+    if type_ == "htm": type_ = "html"
 
-    name = str(path.relative_to(base_path).with_suffix(""))
-    name = name.upper().replace("\\", "/")
-
-    return name, "html" if type_ == "htm" else type_
+    name = str(path.relative_to(base_path).with_suffix("")).upper().replace("\\", "/")
+    return name, type_
