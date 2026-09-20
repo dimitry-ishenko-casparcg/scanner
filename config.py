22e43993109b5cde8e7eefdb27072e003a5af49d
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-09-20 09:51:25 -0400

    config: simplify

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
+from lxml import etree
 from pathlib import Path
-import xml.etree.ElementTree as et
 
 class Config:
     def __init__(self, config_path: Path):
@@ -7,19 +7,15 @@ class Config:
         self.http_addr = "0.0.0.0"
         self.http_port = 8000
 
-        def get_path(config_path: Path, parent, tag, fallback):
-            node = parent.find(tag) if parent else None
-            path = node.text.strip() if node is not None and node.text else fallback
-
-            # Compute 'path' relative to 'config_path'.
-            # If 'path' is absolute, 'config_path' will be ignored.
-            return config_path.parent / Path(path)
-
-        root = et.parse(config_path).getroot()
+        root = etree.parse(config_path).getroot()
         if root.tag != "configuration":
-            raise ValueError(f"Invalid root tag <{root.tag}>.")
-        paths = root.find("paths")
+            raise ValueError(f"Invalid root tag <{root.tag}>")
 
-        self.font_path = get_path(config_path, paths, "font-path", "./font")
-        self.media_path = get_path(config_path, paths, "media-path", "./media")
-        self.template_path = get_path(config_path, paths, "template-path", "./template")
+        def get_path(tag, fallback):
+            val = root.findtext(f"paths/{tag}", default="").strip()
+            # If 'Path(...)' is absolute, 'config_path' will be ignored.
+            return config_path.parent / Path(val or fallback)
+
+        self.font_path = get_path("font-path", "./font")
+        self.media_path = get_path("media-path", "./media")
+        self.template_path = get_path("template-path", "./template")
